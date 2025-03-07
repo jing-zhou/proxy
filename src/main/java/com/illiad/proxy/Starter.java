c710455d1002a9192602f0edc5281b5ed0a0d21a
jingzhou <jingzhou_wang@hotmail.com>
2025-03-07 20:06:22 +0800

    Initializer, combined into Starter

--- src/main/java/com/illiad/proxy/Starter.java
+++ src/main/java/com/illiad/proxy/Starter.java
@@ -1,11 +1,15 @@
 package com.illiad.proxy;
 
-import com.illiad.config.Initializer;
 import com.illiad.config.Params;
+import com.illiad.handler.CommandHandler;
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.socksx.SocksPortUnificationServerHandler;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
 import org.springframework.stereotype.Component;
@@ -13,7 +17,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class Starter {
 
-    public Starter(Params params, Initializer initializer) {
+    public Starter(Params params, CommandHandler commandHandler) {
         // Configure the bootstrap.
         EventLoopGroup bossGroup = new NioEventLoopGroup(3);
         EventLoopGroup workerGroup = new NioEventLoopGroup(4);
@@ -22,8 +26,17 @@ public class Starter {
             b.group(bossGroup, workerGroup)
                     .channel(NioServerSocketChannel.class)
                     .handler(new LoggingHandler(LogLevel.INFO))
-                    .childHandler(initializer)
-                    .bind(params.getLocalPort()).sync().channel().closeFuture().sync();
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) {
+                            ChannelPipeline pipeline = ch.pipeline();
+                            pipeline.addLast(
+                                    new LoggingHandler(LogLevel.INFO),
+                                    new SocksPortUnificationServerHandler(),
+                                    commandHandler);
+                        }
+                    });
+            b.bind(params.getLocalPort()).sync().channel().closeFuture().sync();
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         } finally {
