1e23d24ab4213dfb951f24e1cd7c98aa7c6d2db1
jingzhou <jingzhou_wang@hotmail.com>
2025-03-07 20:48:47 +0800

    HeaderEncoder: ctx.pipeline().remove(this)

--- src/main/java/com/illiad/codec/HeaderEncoder.java
+++ src/main/java/com/illiad/codec/HeaderEncoder.java
@@ -4,15 +4,15 @@ import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToByteEncoder;
-import io.netty.handler.codec.socksx.v5.Socks5Message;
+import io.netty.handler.codec.socksx.SocksMessage;
 import org.springframework.stereotype.Component;
 
 /**
- * Encodes a client-side {@link Socks5Message} into a {@link ByteBuf}.
+ * Encodes a client-side {@link SocksMessage} into a {@link ByteBuf}.
  */
 @Component
 @ChannelHandler.Sharable
-public class HeaderEncoder extends MessageToByteEncoder<Socks5Message> {
+public class HeaderEncoder extends MessageToByteEncoder<SocksMessage> {
 
     private final Header header;
 
@@ -21,7 +21,7 @@ public class HeaderEncoder extends MessageToByteEncoder<Socks5Message> {
     }
 
     @Override
-    protected void encode(ChannelHandlerContext channelHandlerContext, Socks5Message socks5Message, ByteBuf byteBuf) {
+    protected void encode(ChannelHandlerContext ctx, SocksMessage socksMessage, ByteBuf byteBuf) {
         // write offset into byteBuf
         byte[] offset = header.offset();
         byteBuf.writeByte(offset.length);
@@ -38,6 +38,7 @@ public class HeaderEncoder extends MessageToByteEncoder<Socks5Message> {
             byteBuf.writeByte((length >> 8) & 0xFF);
         }
         byteBuf.writeBytes(secret);
+        ctx.pipeline().remove(this);
     }
 
 }
