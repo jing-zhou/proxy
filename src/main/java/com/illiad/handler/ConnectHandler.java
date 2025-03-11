e1f30bf63bc16b70be980795d19a7693882788a3
jingzhou <jingzhou_wang@hotmail.com>
2025-03-11 11:17:09 +0800

    ConnectHandler: promise.addListener

--- src/main/java/com/illiad/handler/ConnectHandler.java
+++ src/main/java/com/illiad/handler/ConnectHandler.java
@@ -15,6 +15,10 @@ import io.netty.handler.codec.socksx.v4.Socks4ClientEncoder;
 import io.netty.handler.codec.socksx.v5.Socks5ClientEncoder;
 import io.netty.handler.codec.socksx.v5.Socks5CommandRequest;
 import io.netty.handler.codec.socksx.v5.Socks5CommandResponseDecoder;
+import io.netty.handler.ssl.SslHandler;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.FutureListener;
+import io.netty.util.concurrent.Promise;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -37,6 +41,44 @@ public final class ConnectHandler extends SimpleChannelInboundHandler<SocksMessa
     @Override
     public void channelRead0(final ChannelHandlerContext ctx, final SocksMessage message) {
 
+        // defind a promise to handle the connection to the remote server
+        Promise<Channel> promise = ctx.executor().newPromise();
+        promise.addListener(
+                new FutureListener<>() {
+                    @Override
+                    public void operationComplete(Future<Channel> future) {
+                        final Channel frontend = ctx.channel();
+                        final ChannelPipeline frontendPipeline = ctx.pipeline();
+                        final Channel backend = future.getNow();
+                        final ChannelPipeline backendPipeline = backend.pipeline();
+                        if (future.isSuccess()) {
+                            // remove all handlers except SslHandler from backpipeline
+                            for (String name : backendPipeline.names()) {
+                                ChannelHandler handler = backendPipeline.get(name);
+                                if (!(handler instanceof SslHandler)) {
+                                    backendPipeline.remove(name);
+                                }
+                            }
+
+                            // remove all handlers from frontpipeline
+                            for (String name : frontendPipeline.names()) {
+                                frontendPipeline.remove(name);
+                            }
+
+                            // setup Socks direct channel relay between frontend and backend
+                            frontendPipeline.addLast(new RelayHandler(backend, utils));
+                            backendPipeline.addLast(new RelayHandler(frontend, utils));
+                            // add SslHandler to frontpipeline
+                            ctx.channel().config().setAutoRead(true);
+                            ctx.fireChannelRead(message);
+                        } else {
+                            utils.closeOnFlush(ctx.channel());
+                            ctx.fireExceptionCaught(future.cause());
+                        }
+                    }
+                }
+        );
+
         try {
             Bootstrap b = new Bootstrap();
             b.group(ctx.channel().eventLoop())
@@ -52,25 +94,25 @@ public final class ConnectHandler extends SimpleChannelInboundHandler<SocksMessa
                             // and accept any invalid certificates in the client side.
                             // You will need something more complicated to identify both
                             // and server in the real world.
-                            pipeline.addLast(ssl.sslCtx.newHandler(ch.alloc(), params.getRemoteHost(), params.getRemotePort()));
-
-                            // On top of the SSL handler, add the text line codec.
-                            // inbound decoder: standard socks5 command response or socks4 response
-                            pipeline.addLast(message instanceof Socks5CommandRequest ? new Socks5CommandResponseDecoder() : new Socks4ClientDecoder());
-                            // outbound encoder: a header followeed by standard socks5 command request (Connect or UdP)
-                            pipeline.addLast(message instanceof Socks5CommandRequest ? Socks5ClientEncoder.DEFAULT : Socks4ClientEncoder.INSTANCE);
-                            pipeline.addLast(headerEncoder);
+                            pipeline.addLast(ssl.sslCtx.newHandler(ch.alloc(), params.getRemoteHost(), params.getRemotePort()),
+                                    // backend inbound decoder: standard socks5 command response or socks4 command response
+                                    message instanceof Socks5CommandRequest ? new Socks5CommandResponseDecoder() : new Socks4ClientDecoder(),
+                                    new ResponseHandler(promise),
+                                    // backend outbound encoder: a header followeed by standard socks5 command request (Connect or UdP) or sock
+                                    message instanceof Socks5CommandRequest ? Socks5ClientEncoder.DEFAULT : Socks4ClientEncoder.INSTANCE,
+                                    headerEncoder);
                         }
                     });
 
+            // connect to the proxy server, and forward the Socks connect command message to the remote server
             b.connect(params.getRemoteHost(), params.getRemotePort())
                     .sync().channel()
                     .writeAndFlush(message)
                     .sync();
 
             ctx.pipeline().remove(this);
-        } catch (Exception ignored) {
-
+        } catch (Exception e) {
+            ctx.fireExceptionCaught(e);
         } finally {
             utils.closeOnFlush(ctx.channel());
         }
@@ -80,5 +122,6 @@ public final class ConnectHandler extends SimpleChannelInboundHandler<SocksMessa
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
         utils.closeOnFlush(ctx.channel());
+        ctx.fireExceptionCaught(cause);
     }
 }
