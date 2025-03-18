7aa5c36f38884443fa2f57441d290bb7175608c6
jingzhou <jingzhou_wang@hotmail.com>
2025-03-18 14:50:36 +0800

    PwdAuthReqDecoder

---
+++ src/main/java/com/illiad/codec/v5/PwdAuthReqDecoder.java
@@ -0,0 +1,78 @@
+package com.illiad.codec.v5;
+
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.DecoderException;
+import io.netty.handler.codec.DecoderResult;
+import io.netty.handler.codec.ReplayingDecoder;
+import io.netty.handler.codec.socksx.v5.DefaultSocks5PasswordAuthRequest;
+import io.netty.handler.codec.socksx.v5.Socks5Message;
+import io.netty.handler.codec.socksx.v5.Socks5PasswordAuthRequest;
+import io.netty.util.CharsetUtil;
+
+import java.util.List;
+
+/**
+ * Decodes a single {@link Socks5PasswordAuthRequest} from the inbound {@link ByteBuf}s.
+ * On successful decode, this decoder will forward the received data to the next handler, so that
+ * other handler can remove or replace this decoder later.  On failed decode, this decoder will
+ * discard the received data, so that other handler closes the connection later.
+ */
+public class PwdAuthReqDecoder extends ReplayingDecoder<State> {
+
+    public PwdAuthReqDecoder() {
+        super(State.INIT);
+    }
+
+    @Override
+    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) {
+        try {
+            switch (state()) {
+                case INIT: {
+                    final int startOffset = in.readerIndex();
+                    final byte version = in.getByte(startOffset);
+                    if (version != 1) {
+                        throw new DecoderException("unsupported subnegotiation version: " + version + " (expected: 1)");
+                    }
+
+                    final int usernameLength = in.getUnsignedByte(startOffset + 1);
+                    final int passwordLength = in.getUnsignedByte(startOffset + 2 + usernameLength);
+                    final int totalLength = usernameLength + passwordLength + 3;
+
+                    in.skipBytes(totalLength);
+                    out.add(new DefaultSocks5PasswordAuthRequest(
+                            in.toString(startOffset + 2, usernameLength, CharsetUtil.US_ASCII),
+                            in.toString(startOffset + 3 + usernameLength, passwordLength, CharsetUtil.US_ASCII)));
+
+                    checkpoint(State.SUCCESS);
+                }
+                case SUCCESS: {
+                    int readableBytes = actualReadableBytes();
+                    if (readableBytes > 0) {
+                        out.add(in.readRetainedSlice(readableBytes));
+                    }
+                    break;
+                }
+                case FAILURE: {
+                    in.skipBytes(actualReadableBytes());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            fail(out, e);
+        }
+    }
+
+    private void fail(List<Object> out, Exception cause) {
+        if (!(cause instanceof DecoderException)) {
+            cause = new DecoderException(cause);
+        }
+
+        checkpoint(State.FAILURE);
+
+        Socks5Message m = new DefaultSocks5PasswordAuthRequest("", "");
+        m.setDecoderResult(DecoderResult.failure(cause));
+        out.add(m);
+    }
+}
