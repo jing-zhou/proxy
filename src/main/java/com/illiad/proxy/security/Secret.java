c79cdf4b6291d43e5a8e5d3fa725dedc050b11e5
jingzhou <jingzhou_wang@hotmail.com>
2025-03-23 17:13:20 +0800

    Secret: acquire secret byte array

--- src/main/java/com/illiad/proxy/security/Secret.java
+++ src/main/java/com/illiad/proxy/security/Secret.java
@@ -3,14 +3,13 @@ package com.illiad.proxy.security;
 import com.illiad.proxy.config.Params;
 import lombok.Getter;
 import org.springframework.stereotype.Component;
-
-import java.util.HexFormat;
+import java.nio.charset.StandardCharsets;
 
 @Component
 public class Secret {
     private final Params params;
     @Getter
-    private byte[] secret = HexFormat.of().parseHex("password");
+    private byte[] secret = "password".getBytes(StandardCharsets.UTF_8);
 
     public Secret(Params params) {
         this.params = params;
