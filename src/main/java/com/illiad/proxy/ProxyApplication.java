d0c298b35a40a8d62ddd4ec98d4a616415057c39
jingzhou <jingzhou_wang@hotmail.com>
2025-03-31 21:48:20 +0800

    ProxyApplication: ComponentScan

--- src/main/java/com/illiad/proxy/ProxyApplication.java
+++ src/main/java/com/illiad/proxy/ProxyApplication.java
@@ -2,8 +2,10 @@ package com.illiad.proxy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.illiad.proxy"})
 public class ProxyApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProxyApplication.class, args);
