4fb8bc8f186252fd07588e62e48974abb846109d
jingzhou <jingzhou_wang@hotmail.com>
2025-05-07 12:16:35 +0800

    Param: crypto property

--- src/main/java/com/illiad/proxy/config/Params.java
+++ src/main/java/com/illiad/proxy/config/Params.java
@@ -11,8 +11,9 @@ public class Params {
     int localPort = Integer.parseInt(System.getProperty("localPort", "2080"));
     String remoteHost = System.getProperty("remoteHost", "www.google.com");
     int remotePort = Integer.parseInt(System.getProperty("remotePort", "443"));
-    int min = Integer.parseInt(System.getProperty("min", "1"));
-    int max = Integer.parseInt(System.getProperty("max", "128"));
+    String crypto = System.getProperty("crypto", "SHA-256");
+    int min = Integer.parseInt(System.getProperty("min", "8"));
+    int max = Integer.parseInt(System.getProperty("max", "256"));
     String secret = "password";
 
 }
