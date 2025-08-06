2e466a5ac10334dcaf0701456760e9f2180755d4
jingzhou <jingzhou_wang@hotmail.com>
2025-08-06 14:00:10 +0800

    UdpChannel: "dChannel" as "NioDatagramChannel"

--- src/main/java/com/illiad/proxy/UdpChannel.java
+++ src/main/java/com/illiad/proxy/UdpChannel.java
@@ -1,12 +1,12 @@
 package com.illiad.proxy;
 
+import io.netty.channel.socket.nio.NioDatagramChannel;
 import org.springframework.stereotype.Component;
-import java.nio.channels.DatagramChannel;
 
 /**
  * this class hold an instance of UDP relay channel on server side
  */
 @Component
 public class UdpChannel {
-    public DatagramChannel dChannel = null;
+    public NioDatagramChannel dChannel = null;
 }
