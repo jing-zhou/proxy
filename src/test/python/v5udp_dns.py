926a124b0f11f5dec0d959a514f8057cd391e963
jingzhou <jingzhou_wang@hotmail.com>
2025-08-28 20:15:12 +0800

    v5udp_dns: adapt for test

--- src/test/python/v5udp_dns.py
+++ src/test/python/v5udp_dns.py
@@ -4,9 +4,12 @@ import struct
 PROXY_HOST = '127.0.0.1'
 PROXY_PORT = 3080
 
-TARGET_HOST = '8.8.8.8'
-TARGET_PORT = 53
-DNS_QUERY = b'\x12\x34\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00\x03www\x06google\x03com\x00\x00\x01\x00\x01'
+# TARGET_HOST = '8.8.8.8'
+# TARGET_PORT = 53
+# DNS_QUERY = b'\x12\x34\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00\x03www\x06google\x03com\x00\x00\x01\x00\x01'
+TARGET_HOST = '127.0.0.1'
+TARGET_PORT = 5005
+DNS_QUERY = "Hello, UDP Server!".encode('utf-8')
 
 # SOCKS5 UDP request header
 # +----+------+------+----------+----------+----------+
@@ -37,7 +40,7 @@ def socks5_udp_associate(proxy_host, proxy_port):
     s.close()
     return relay_ip, relay_port
 
-def build_socks5_udp_packet(ip, port, data):
+def socks5_udp_packet(ip, port, data):
     rsv = b'\x00\x00'
     frag = b'\x00'
     atyp = b'\x01'
@@ -49,8 +52,10 @@ def main():
     relay_ip, relay_port = socks5_udp_associate(PROXY_HOST, PROXY_PORT)
     print(f'UDP relay at {relay_ip}:{relay_port}')
     udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    udp_sock.settimeout(5)
-    packet = build_socks5_udp_packet(TARGET_HOST, TARGET_PORT, DNS_QUERY)
+    # set a much longer timer when debug, so the UDP channel won't close
+    udp_sock.settimeout(600)
+    # udp_sock.settimeout(5)
+    packet = socks5_udp_packet(TARGET_HOST, TARGET_PORT, DNS_QUERY)
     udp_sock.sendto(packet, (relay_ip, relay_port))
     try:
         data, addr = udp_sock.recvfrom(4096)
