41de1fd7f1300f9d684eeb1f2254fa4a75cc6812
jingzhou <jingzhou_wang@hotmail.com>
2025-10-28 15:40:09 +0800

    Starter: rewrite to have socks http co-existing

--- src/main/java/com/illiad/proxy/Starter.java
+++ src/main/java/com/illiad/proxy/Starter.java
@@ -3,6 +3,7 @@ package com.illiad.proxy;
 import com.illiad.proxy.handler.http.FrontHandler;
 import com.illiad.proxy.handler.v5.VersionHandler;
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.EventLoopGroup;
@@ -13,74 +14,85 @@ import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.http.HttpServerCodec;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class Starter {
 
     private final ParamBus bus;
+    private final List<Channel> channels = new ArrayList<>();
+
+    // Create separate, independent EventLoopGroups for each server
+    private final EventLoopGroup socksBossGroup = new NioEventLoopGroup(1);
+    private final EventLoopGroup socksWorkerGroup = new NioEventLoopGroup(2);
+
+    private final EventLoopGroup httpBossGroup = new NioEventLoopGroup(1);
+    private final EventLoopGroup httpWorkerGroup = new NioEventLoopGroup(2);
 
     public Starter(ParamBus bus) {
         this.bus = bus;
+    }
 
+    @PostConstruct
+    public void startServers() throws InterruptedException {
         startSocks();
-        // Http server is disabled because it disables socks5 somwhow
-        // startHttp();
-        //TODO: have socks and http run simultaneously
+        startHttp();
+    }
+
+    @PreDestroy
+    public void stopServers() {
+        System.out.println("Shutting down Netty servers...");
+        channels.forEach(Channel::close);
+        socksWorkerGroup.shutdownGracefully();
+        socksBossGroup.shutdownGracefully();
+        httpWorkerGroup.shutdownGracefully();
+        httpBossGroup.shutdownGracefully();
     }
 
     /**
      * Socks5 starter
      */
-    private void startSocks() {
-        // Configure the bootstrap.
-        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
-        EventLoopGroup workerGroup = new NioEventLoopGroup(2);
-        try {
-            ServerBootstrap bs = new ServerBootstrap();
-            bs.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
-                    .childHandler(new ChannelInitializer<NioSocketChannel>() {
-                        @Override
-                        protected void initChannel(NioSocketChannel ch) {
-                            ChannelPipeline pipeline = ch.pipeline();
-                            pipeline.addLast(new LoggingHandler(LogLevel.INFO),
-                                    new VersionHandler(bus));
-                        }
-                    });
-            bs.bind(bus.params.getLocalPort()).sync().channel().closeFuture().sync();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        } finally {
-            bossGroup.shutdownGracefully();
-            workerGroup.shutdownGracefully();
-        }
+    private void startSocks() throws InterruptedException {
+        ServerBootstrap bs = new ServerBootstrap();
+        bs.group(socksBossGroup, socksWorkerGroup)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) {
+                        ChannelPipeline pipeline = ch.pipeline();
+                        pipeline.addLast(new LoggingHandler(LogLevel.INFO),
+                                new VersionHandler(bus));
+                    }
+                });
+
+        Channel channel = bs.bind(bus.params.getLocalPort()).sync().channel();
+        channels.add(channel);
+        System.out.println("SOCKS5 server started on port " + bus.params.getLocalPort());
     }
 
     /**
      * HTTP starter
      */
-    private void startHttp() {
-        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
-        EventLoopGroup workerGroup = new NioEventLoopGroup(2);
-        try {
-            ServerBootstrap bh = new ServerBootstrap();
-            bh.group(bossGroup, workerGroup)
-                    .channel(NioServerSocketChannel.class)
-                    .handler(new LoggingHandler(LogLevel.INFO))
-                    .childHandler(new ChannelInitializer<SocketChannel>() {
-                        @Override
-                        public void initChannel(SocketChannel ch) {
-                            ch.pipeline().addLast(bus.namer.generateName(), new HttpServerCodec())
-                                    .addLast(bus.namer.generateName(), new FrontHandler(bus));
-                        }
-                    });
+    private void startHttp() throws InterruptedException {
+        ServerBootstrap bh = new ServerBootstrap();
+        bh.group(httpBossGroup, httpWorkerGroup)
+                .channel(NioServerSocketChannel.class)
+                .handler(new LoggingHandler(LogLevel.INFO))
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    public void initChannel(SocketChannel ch) {
+                        ch.pipeline().addLast(bus.namer.generateName(), new HttpServerCodec())
+                                .addLast(bus.namer.generateName(), new FrontHandler(bus));
+                    }
+                });
 
-            bh.bind(bus.params.getHttpPort()).sync().channel().closeFuture().sync();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        } finally {
-            bossGroup.shutdownGracefully();
-            workerGroup.shutdownGracefully();
-        }
+        Channel channel = bh.bind(bus.params.getHttpPort()).sync().channel();
+        channels.add(channel);
+        System.out.println("HTTP server started on port " + bus.params.getHttpPort());
     }
 }
